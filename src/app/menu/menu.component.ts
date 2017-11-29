5745db93e410cd73eadbe13d480277df231930bb
Shiva Kumar <shivu104@gmail.com>
2017-11-29 19:28:46 +0530

    all days in one page
    all days in one page

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
-   days = ['4 Dec Monday','5 Dec Tuesday','6 Dec Wednesday','7 Dec Thursday','8 Dec Friday'];
+   daysCount = [1,2,3,4,5];
+   dayDetails= {1:{'day':'Monday','date':'4 Dec'},
+2:{'day':'Tuesday','date':'5 Dec'},
+3:{'day':'Wednesday','date':'6 Dec'},
+4:{'day':'Thursday','date':'7 Dec'},
+5:{'day':'Friday','date':'8 Dec'}};
+
    currentDay = 1;
    currentMenu = 1;
    likes= {'1_1':0,'2_1':0,'3_1':0,'4_1':0,'5_1':0,'1_2':0,'2_2':0,'3_2':0,'4_2':0,'5_2':0};
@@ -18,36 +24,66 @@ export class MenuComponent {
 'https://content1.jdmagicbox.com/comp/bangalore/q7/080pxx80.xx80.160304101252.d4q7/catalogue/indian-food-court-electronic-city-bangalore-home-delivery-restaurants-z117kl.jpg',
 'https://media-cdn.tripadvisor.com/media/photo-s/0e/c6/38/1c/curry-magic-indian-food.jpg',
 'https://riceandcurry.files.wordpress.com/2012/02/dscn2554.jpg'],
-'2_1':['https://assets.londonist.com/uploads/2016/05/i875/jashan_vegetarian_thali_1.jpg'],
-'3_1':['http://del.h-cdn.co/assets/17/19/1494616646-maryland-.jpg'],
-'4_1':['https://pauperswithouttravel.files.wordpress.com/2012/07/rajdhani3.jpeg'],
-'5_1':['https://i.ytimg.com/vi/f3OsUwuWxgg/maxresdefault.jpg'],
-'1_2':['https://upload.wikimedia.org/wikipedia/commons/8/86/Marwadi_Gujarati_Thali.jpg'],
-'2_2':['https://upload.wikimedia.org/wikipedia/commons/7/70/India_ki_Thali.jpg'],
-'3_2':['http://www.jauntlust.com/wp-content/uploads/2016/08/South-Indian-Food.jpg'],
-'4_2':['https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Sadhya_DSW.jpg/1200px-Sadhya_DSW.jpg'],
-'5_2':['https://i.pinimg.com/736x/2a/90/6c/2a906c9e1b02936860c20ab30ac23889--andhra-recipes-indian-recipes.jpg']};
+'2_1':['https://media-cdn.tripadvisor.com/media/photo-s/0b/71/97/4a/north-indian-food-kaitaia.jpg',
+'https://indianhealthyrecipes.com/wp-content/uploads/2017/08/paneer-lababdar.jpg',
+'http://www.maavalanindiatravels.com/wp-content/uploads/2016/01/Best-Indian-Food-dal-makhani.jpg',
+'https://indianhealthyrecipes.com/wp-content/uploads/2017/10/bhindi-masala-recipe-348x232.jpg'],
+'3_1':['https://www.fitfoodie.in/sites/default/files/02_0.jpg',
+'https://media-cdn.tripadvisor.com/media/photo-s/0b/71/97/4d/north-indian-food-kaitaia.jpg',
+'http://www.foodofy.com/wp-content/uploads/2015/07/north-indian-foods-15.jpg',
+'https://www.fitfoodie.in/sites/default/files/08_5.jpg',
+'http://www.cityshor.com/uploads/article/09_2016/1475040439_W_8_.jpg'],
+'4_1':['http://bongong.com/images/recipes/Spicy_Onion_Rice/IMG_0031_1243361826_center.jpg',
+'http://justindianfood.com/wp-content/uploads/2013/12/cholemasala1.jpg',
+'https://i2.wp.com/blog.getfitso.com/wp-content/uploads/2016/10/North-Indian-Review-3-e1477471894462.jpg?resize=648%2C280',
+'http://nu.tree.ooo/wp-content/uploads/2017/07/p3.jpg',
+'https://cf0316.s3.amazonaws.com/cookificom/dishes/47/e40dab.jpg'],
+'5_1':['http://english.mathrubhumi.com/polopoly_fs/1.1818744.1490269339!/image/image.jpg_gen/derivatives/landscape_822/image.jpg',
+'https://media-cdn.tripadvisor.com/media/photo-s/0e/c6/38/4c/curry-magic-indian-food.jpg',
+'https://bizimages.withfloats.com/actual/59e595b400ccd60544c00712.jpg',
+'https://images.mapsofindia.com/my-india/2014/09/mava-kachori.jpg',
+'https://i1.wp.com/blog-guru.net/wp-content/uploads/2015/11/Best-North-Indian-Food-Recipe3.jpg?ssl=1'],
+'1_2':['https://www.whatsuplife.in/kolkata/blog/wp-content/uploads/2016/07/13971557301397600649-1024x678.jpg',
+'http://hungrydunia.com/upload/0/20170203122043_0_paneer.jpg',
+'http://www.namskar.ca/wp-content/uploads/2017/06/sahi-paneer-web-1046x470.jpg',
+'https://i.ytimg.com/vi/HiciJDlGDbY/maxresdefault.jpg'],
+'2_2':['https://www.boldsky.com/img/2016/04/22-1461298892-pic3.jpg',
+'https://images.click.in/classifieds/images/151/17274286_28_6_2016_22_48_54_386859807_7ampemb9j.jpg',
+'https://content1.jdmagicbox.com/comp/bangalore/q7/080pxx80.xx80.160304101252.d4q7/catalogue/indian-food-court-electronic-city-bangalore-home-delivery-restaurants-z117kl.jpg',
+'http://cdn.c.photoshelter.com/img-get/I00009zUbNdQub.4/s/650/650/Chicken-Madras-Indian-Food-Pictures-PWP19274.jpg'],
+'3_2':['http://indiafoods.india-biz.in/foods/images/naan1.jpg',
+'https://www.vahrehvah.com/indianfood_org/wp-content/uploads/2010/07/eU3ebuvIr31.jpg',
+'http://3.bp.blogspot.com/-EmUP98MLGr4/Uuu4fBsC53I/AAAAAAAAK9w/dN5CKPpxdGQ/s1600/Aloo+Matar+Pulao.jpg',
+'http://www.sangitaskitchen.com/wp-content/uploads/2014/03/palak-paneer11.jpg'],
+'4_2':['http://www.vegrecipesofindia.com/wp-content/uploads/2015/01/hing-jeera-aloo-recipe1.jpg',
+'http://3.bp.blogspot.com/-x3BXVLRhHsc/UGyCViYg0jI/AAAAAAAAAnw/ZwaHsZxxQHo/s1600/North+Indian+food+recipes+for+Rakhi.JPG',
+'https://myannoyingopinions.files.wordpress.com/2014/11/rajma4.jpg?w=869',
+'https://i.ytimg.com/vi/HiciJDlGDbY/maxresdefault.jpg'],
+'5_2':['http://www.vegrecipesofindia.com/wp-content/uploads/2015/01/hing-jeera-aloo-recipe1.jpg',
+'http://3.bp.blogspot.com/-x3BXVLRhHsc/UGyCViYg0jI/AAAAAAAAAnw/ZwaHsZxxQHo/s1600/North+Indian+food+recipes+for+Rakhi.JPG',
+'https://myannoyingopinions.files.wordpress.com/2014/11/rajma4.jpg?w=869',
+'https://i.ytimg.com/vi/HiciJDlGDbY/maxresdefault.jpg']};
 
    changeDay(item){
      this.currentDay = (item.index)+1;
      console.log(item);
    }
-   getLikes(menuNumber){
-     return this.likes[this.currentDay+'_'+menuNumber];
+   getLikes(dayNumber,menuNumber){
+     return this.likes[dayNumber+'_'+menuNumber];
    }
-   getDislikes(menuNumber){
-     return this.dislikes[this.currentDay+'_'+menuNumber];
+   getDisLikes(dayNumber,menuNumber){
+     return this.dislikes[dayNumber+'_'+menuNumber];
    }
-   like(menuNumber){
-     this.likes[this.currentDay+'_'+menuNumber]= this.likes[this.currentDay+'_'+menuNumber]+1;
+   like(dayNumber,menuNumber){
+     this.likes[dayNumber+'_'+menuNumber]= this.likes[dayNumber+'_'+menuNumber]+1;
    }
-   disLike(menuNumber){
-     this.dislikes[this.currentDay+'_'+menuNumber]= this.dislikes[this.currentDay+'_'+menuNumber]+1;
+   disLike(dayNumber,menuNumber){
+     this.dislikes[dayNumber+'_'+menuNumber]= this.dislikes[dayNumber+'_'+menuNumber]+1;
    }
    changeMenu(){
      this.currentMenu= ((this.currentMenu)==1)?2:1;
    }
-   getMenu(menuNumber){
-     return this.menus[this.currentDay+'_'+menuNumber];
+   getMenu(dayNumber, menuNumber){
+     return this.menus[dayNumber+'_'+menuNumber];
    }
 }
