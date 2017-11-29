72b6afb07c12b15a84a0d65011aa541daace6f2e
Deepak Dinakara <ddinakar@akamai.com>
2017-11-29 17:53:59 +0530

    CSS Updates

--- src/app/roster/roster.component.ts
+++ src/app/roster/roster.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-roster',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RosterComponent implements OnInit {
 
-  constructor() { }
+  constructor(public snackBar: MatSnackBar) { }
 
   ngOnInit() {
+  }
+
+  onSaveRoster() {
+    this.snackBar.open('Roster Updated!', 'Success', {
+      duration: 1000,
+      panelClass: 'success-color'
+    });
   }
 
 }
